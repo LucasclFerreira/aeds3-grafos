2622aadefda5b2ba00470e85212d70caccded82b
LucasclFerreira <lucascosta.ferreira@sou.unifal-mg.edu.br>
2023-04-26 13:14:05 -0300

    remocao de arestas implementado

--- lista-adjacencia/lista-adjacencia.cpp
+++ lista-adjacencia/lista-adjacencia.cpp
@@ -20,11 +20,42 @@ class Grafo {
         }
 
         void remover_aresta(int v1, int v2) {
-            lista_adj[v1].erase(remove(lista_adj[v1].begin(), lista_adj[v1].end(), v2), lista_adj[v1].end());
-            lista_adj[v2].erase(v1);
+            int i;
+            for (i = 0; i < lista_adj[v1].size(); i++) {
+                if (lista_adj[v1][i] == v2) {
+                    lista_adj[v1].erase(lista_adj[v1].begin() + i);
+                    break;
+                }
+            }
+
+            for (i = 0; i < lista_adj[v2].size(); i++) {
+                if (lista_adj[v2][i] == v1) {
+                    lista_adj[v2].erase(lista_adj[v2].begin() + i);
+                    break;
+                }
+            }
+        }
+
+        void mostrar_lista() {
+            for (int i = 0; i < this->num_vertices; i++) {
+                printf("\nVertice %d:", i);
+                for (auto vert : lista_adj[i])
+                    printf(" -> %d", vert);
+                printf("\n");
+            }
         }
 };
 
 int main() {
+    Grafo grafo = Grafo(8);
+    grafo.adicionar_aresta(0, 1);
+    grafo.adicionar_aresta(0, 2);
+    grafo.adicionar_aresta(2, 3);
+    grafo.adicionar_aresta(3, 4);
+    grafo.adicionar_aresta(3, 5);
+    grafo.adicionar_aresta(4, 5);
+    grafo.adicionar_aresta(4, 6);
+    grafo.adicionar_aresta(6, 7);
 
+    grafo.mostrar_lista();
 }
